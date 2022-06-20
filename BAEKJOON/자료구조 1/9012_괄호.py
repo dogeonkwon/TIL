25dc1d995502990dd84e20636f08d7e02b8be994
unknown <rnsjquddbs@gmail.com>
2022-06-20 11:47:36 +0900

    BAEKJOON Problem Solving

--- BAEKJOON/자료구조 1/9012_괄호.py
+++ BAEKJOON/자료구조 1/9012_괄호.py
@@ -4,11 +4,10 @@ import sys
 n = int(sys.stdin.readline())
 for _ in range(n):
     balance = 0
-    bracket = sys.stdin.readline()
-    
-    if bracket[-1] == '(' or bracket[0] == ')':
+    bracket = input()       # sys.stdin.readline()의 경우 괄호 맨마지막을 문자열로 인식하지 못함, 따라서 input() 사용
+    if bracket.strip()[-1] == '(' or bracket[0] == ')':
         print('NO')
-        break
+        continue
     for i in range(len(bracket)):
         if bracket[i] == '(':
             balance += 1
