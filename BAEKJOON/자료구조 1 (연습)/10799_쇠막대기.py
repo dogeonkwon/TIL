66de35d1507da8841c72281dee721b3f95b37ef4
unknown <rnsjquddbs@gmail.com>
2022-06-28 12:41:22 +0900

    BAEKJOON Problem Solving

--- BAEKJOON/자료구조 1 (연습)/10799_쇠막대기.py
+++ BAEKJOON/자료구조 1 (연습)/10799_쇠막대기.py
@@ -1,2 +1,16 @@
 import sys
 
+bracket = sys.stdin.readline()
+n = len(bracket)
+m = ans = 0
+for i in range(n):      # 괄호를 검사하여 (( 이 연속으로 나오면 ans += 1, m에 1씩 추가
+    if bracket[i] == '(':
+        if bracket[i+1] == '(':
+            ans += 1
+            m += 1
+        else:           # )이 나온다면 ans += m
+            ans += m
+    else:
+        if bracket[i-1] == ')':     # )) 이 나왔다면 m -= 1
+            m -= 1
+print(ans)
