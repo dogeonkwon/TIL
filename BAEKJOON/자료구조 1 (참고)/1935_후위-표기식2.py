ffccec432624b2b3e01d8e3722727a7d347c6fdb
unknown <rnsjquddbs@gmail.com>
2022-07-02 23:27:25 +0900

    BAEKJOON Problem Solving

--- BAEKJOON/자료구조 1 (참고)/1935_후위-표기식2.py
+++ BAEKJOON/자료구조 1 (참고)/1935_후위-표기식2.py
@@ -1,13 +1,26 @@
 import sys
 
-# n = int(sys.stdin.readline())
-# string = sys.stdin.readline().strip()
+n = int(sys.stdin.readline())
+string = sys.stdin.readline().strip()
 
-print(ord('A'))
-print(ord('Z'))
+num = dict()
+for i in range(65, 65+n):
+    num[chr(i)] = int(sys.stdin.readline())
 
+stack = list()
+for j in string:
+    if j in ['+', '-', '*', '/']:
+        right = stack.pop()
+        left = stack.pop()
+        if j == '+':
+            stack.append(left + right)
+        elif j == '-':
+            stack.append(left - right)
+        elif j == '*':
+            stack.append(left * right)
+        elif j == '/':
+            stack.append(left / right)
+    else:
+        stack.append(num[j])
 
-# for i in range(n):
-
-
-
+print(f'{stack[0]:.2f}')
