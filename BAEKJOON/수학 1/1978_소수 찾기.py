6a70acf44589317003e48c11c5720b01ed9d8073
unknown <rnsjquddbs@gmail.com>
2022-07-06 16:57:26 +0900

    BAEKJOON Problem Solving

--- BAEKJOON/수학 1/1978_소수 찾기.py
+++ BAEKJOON/수학 1/1978_소수 찾기.py
@@ -1,3 +1,12 @@
 import sys
 
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(n):
+        if n % i == 0:
+            return False
+    return True
+
+
 n = int(sys.stdin.readline())
