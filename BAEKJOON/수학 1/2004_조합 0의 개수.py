fb31fb16f19aba834be9377f3f3c0713dc7f8bd7
unknown <rnsjquddbs@gmail.com>
2022-07-10 13:08:39 +0900

    BAEKJOON Problem Solving

--- BAEKJOON/수학 1/2004_조합 0의 개수.py
+++ BAEKJOON/수학 1/2004_조합 0의 개수.py
@@ -1,13 +1,22 @@
 import sys
-import math
+
+
+def f_count(s):
+    res = 0
+    while s != 0:
+        s //= 5
+        res += s
+    return res
+
+
+def t_count(s):
+    res = 0
+    while s != 0:
+        s //= 2
+        res += s
+    return res
 
 
 n, m = map(int, sys.stdin.readline().split())
-fac = int(math.factorial(n) / (math.factorial(m) * math.factorial(n-m)))
-ans = 0
-for i in str(fac)[::-1]:
-    if i == '0':
-        ans += 1
-    else:
-        print(ans)
-        break
+ans = min(f_count(n) - f_count(m) - f_count(n-m), t_count(n) - t_count(m) - t_count(n-m))
+print(ans)
