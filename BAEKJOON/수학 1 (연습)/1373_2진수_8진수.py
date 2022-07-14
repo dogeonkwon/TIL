d55a73a522e6b2bc531ff86a9f8f1074e8edd4c1
권도건 <kjmk1007@naver.com>
2022-07-14 22:52:08 +0900

    BOJ Problem Solving

--- BAEKJOON/수학 1 (연습)/1373_2진수_8진수.py
+++ BAEKJOON/수학 1 (연습)/1373_2진수_8진수.py
@@ -1,6 +1,20 @@
 import sys
 
-n = sys.stdin.readline()
+n = sys.stdin.readline().strip()
 
-for i in range(n):
-    
+k = len(n)-1
+num = 0
+s = ((k // 3) + 1)
+ans = [0] * s
+for i in n:
+    if i == '1':
+        if k >= 3:
+            ans[k//3] += (k % 3) + 1
+        else:
+            ans[k//3] += 2**k
+    k -= 1
+
+res = ''
+for j in ans[::-1]:
+    res += str(j)
+print(res)
