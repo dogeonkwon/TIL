431eb2c4a84c2e5437d07f404e6a2454efd1c336
권도건 <rnsjquddbs@gmail.com>
2022-08-05 00:24:50 +0900

    BOJ Problem Solving

--- BAEKJOON/수학 1 (연습)/2089_-2진수.py
+++ BAEKJOON/수학 1 (연습)/2089_-2진수.py
@@ -1,5 +1,17 @@
 import sys
 
-n = sys.stdin.readline()
+n = int(sys.stdin.readline())
+ans = ''
 
-print(n)
+while n != 0:
+    if abs(n) % 2:
+       ans += '1'
+       n = (n // -2) + 1
+    else:
+        ans += '0'
+        n = (n // -2)
+
+if ans == '':
+    print(0)
+else:
+    print(ans[::-1])
