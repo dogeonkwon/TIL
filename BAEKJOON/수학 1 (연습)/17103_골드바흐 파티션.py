f282d32bf037cadd3f3487035493c3797c1d8771
권도건 <rnsjquddbs@gmail.com>
2022-08-07 00:50:12 +0900

    BOJ Problem Solving

--- BAEKJOON/수학 1 (연습)/17103_골드바흐 파티션.py
+++ BAEKJOON/수학 1 (연습)/17103_골드바흐 파티션.py
@@ -1,6 +1,28 @@
 import sys
 
-tc = int(sys.stdin.readline().strip())
+tc = int(sys.stdin.readline())
 
-# 2의 배수?
-for _ in range(tc):
+nums = [0] * tc
+maxN = 0
+for l in range(tc):
+    k = int(sys.stdin.readline())
+    nums[l] = k
+
+    if maxN < k:
+        maxN = k
+
+prime = [True] * (maxN + 1)
+prime[1] = False
+
+for i in range(2, int(maxN ** 0.5) + 1):
+    if prime[i]:
+        for j in range(i + i, maxN + 1, i):
+            prime[j] = False
+
+for n in nums:
+    ans = 0
+    for m in range(2, n // 2 + 1):
+        if prime[m] and prime[n - m]:
+            ans += 1
+
+    print(ans)
