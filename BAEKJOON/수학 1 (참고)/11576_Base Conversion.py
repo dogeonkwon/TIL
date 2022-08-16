3369f1f0e3f4aa541db28b5138f82349ab40a2ea
권도건 <rnsjquddbs@gmail.com>
2022-08-17 02:37:45 +0900

    BOJ Problem Solving

--- BAEKJOON/수학 1 (참고)/11576_Base Conversion.py
+++ BAEKJOON/수학 1 (참고)/11576_Base Conversion.py
@@ -6,8 +6,18 @@ A, B = map(int, sys.stdin.readline().split())
 # A진법으로 나타낸 숫자의 자리수의 개수
 m = int(sys.stdin.readline())
 
-# A진법을 이루고 있는 숫자 m개가 공백을 구분으로 높은 자릿수부터 차례대로 주어진다.
-lst = list()
-for _ in range(m):
-    lst.append(int(sys.stdin.readline()))
+nums = list(map(int, sys.stdin.readline().split()))[::-1]
 
+numA = 0
+
+# A진법을 이루고 있는 숫자 m개가 공백을 구분으로 높은 자릿수부터 차례대로 주어진다.
+res = list()
+
+for i in range(len(nums)):
+    numA += nums[i] * (A**i)
+
+while numA != 0:
+    res.append(numA % B)
+    numA //= B
+
+print(" ".join(map(str, res[::-1])))
