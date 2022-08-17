b39cabf26948ecd80bf7e73cd587c3739ab79ac9
권도건 <rnsjquddbs@gmail.com>
2022-08-18 00:47:54 +0900

    BOJ Problem Solving

--- BAEKJOON/수학 1 (참고)/11576_Base Conversion.py
+++ BAEKJOON/수학 1 (참고)/11576_Base Conversion.py
@@ -1,16 +1,12 @@
 import sys
 
-# 미래에서 사용하는 A진법 / 정이가 사용하는 B진법
 A, B = map(int, sys.stdin.readline().split())
 
-# A진법으로 나타낸 숫자의 자리수의 개수
 m = int(sys.stdin.readline())
 
 nums = list(map(int, sys.stdin.readline().split()))[::-1]
 
 numA = 0
-
-# A진법을 이루고 있는 숫자 m개가 공백을 구분으로 높은 자릿수부터 차례대로 주어진다.
 res = list()
 
 for i in range(len(nums)):
