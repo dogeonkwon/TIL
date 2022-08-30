581d11505b8e5c04b430a9a9390abf7f1874fe34
권도건 <rnsjquddbs@gmail.com>
2022-08-31 00:45:54 +0900

    BOJ Problem Solving

--- BAEKJOON/다이나믹 프로그래밍 1/1463_1로 만들기.py
+++ BAEKJOON/다이나믹 프로그래밍 1/1463_1로 만들기.py
@@ -1,18 +1,44 @@
 import sys
 
+N = int(sys.stdin.readline())
 
-N = int(sys.stdin.readline().strip())
+arr = [0] * (N+1)
 
-dp = [0 for _ in range(N+1)]
-
-
-dp[1] = 0
 for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
+    arr[i] = arr[i-1] + 1
 
-    if i % 3 == 0 and dp[i//3] + 1 < dp[i]:
-        dp[i] = dp[i//3] + 1
-    if i % 2 == 0 and dp[i//2] + 1 < dp[i]:
-        dp[i] = dp[i//2] + 1
+    if arr[i]
 
-print(dp[N])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# N = int(sys.stdin.readline().strip())
+#
+# dp = [0 for _ in range(N+1)]
+#
+#
+# dp[1] = 0
+# for i in range(2, N+1):
+#     dp[i] = dp[i-1] + 1
+#
+#     if i % 3 == 0 and dp[i//3] + 1 < dp[i]:
+#         dp[i] = dp[i//3] + 1
+#     if i % 2 == 0 and dp[i//2] + 1 < dp[i]:
+#         dp[i] = dp[i//2] + 1
+#
+# print(dp[N])
