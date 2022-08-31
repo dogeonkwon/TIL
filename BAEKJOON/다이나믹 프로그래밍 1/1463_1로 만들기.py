fac44f5b5b5bd87e23d782906a6a3df6ca41ed0d
권도건 <rnsjquddbs@gmail.com>
2022-09-01 08:32:26 +0900

    BOJ Problem Solving

--- BAEKJOON/다이나믹 프로그래밍 1/1463_1로 만들기.py
+++ BAEKJOON/다이나믹 프로그래밍 1/1463_1로 만들기.py
@@ -2,43 +2,17 @@ import sys
 
 N = int(sys.stdin.readline())
 
+# N만큼의 배열을 만들어줌
 arr = [0] * (N+1)
 
 for i in range(2, N+1):
-    arr[i] = arr[i-1] + 1
 
-    if arr[i]
+    arr[i] = arr[i-1] + 1    # 1을 뺄 수 있는 조건이 있으므로 1을 하나씩 더하며 올라감
 
+    if not i % 3 and arr[i] > arr[i//3]:    # X가 3으로 나누어 떨어지고, i가 i//3의 값보다 크다면
+        arr[i] = arr[i//3] + 1        # 작은 값으로 바꿔줌
 
+    if not i % 2 and arr[i] > arr[i//2]:
+        arr[i] = arr[i//2] + 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# N = int(sys.stdin.readline().strip())
-#
-# dp = [0 for _ in range(N+1)]
-#
-#
-# dp[1] = 0
-# for i in range(2, N+1):
-#     dp[i] = dp[i-1] + 1
-#
-#     if i % 3 == 0 and dp[i//3] + 1 < dp[i]:
-#         dp[i] = dp[i//3] + 1
-#     if i % 2 == 0 and dp[i//2] + 1 < dp[i]:
-#         dp[i] = dp[i//2] + 1
-#
-# print(dp[N])
+print(arr[N])
