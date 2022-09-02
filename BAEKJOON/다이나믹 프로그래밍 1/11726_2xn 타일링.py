8d49480ad744bbfc4e663ad0298cff938d552cb4
권도건 <rnsjquddbs@gmail.com>
2022-09-02 09:16:30 +0900

    BOJ Problem Solving

--- BAEKJOON/다이나믹 프로그래밍 1/11726_2xn 타일링.py
+++ BAEKJOON/다이나믹 프로그래밍 1/11726_2xn 타일링.py
@@ -2,9 +2,12 @@ import sys
 
 n = int(sys.stdin.readline())
 
-dp = [0, 1, 2] + [0]*n
+dp = [0] * (n+2)    # n이 1 or 2 일 때를 위해서 n + 2
+dp[1] = 1
+dp[2] = 2
 
-for i in range(3, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+# 피보나치 수열
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n] % 10007)
