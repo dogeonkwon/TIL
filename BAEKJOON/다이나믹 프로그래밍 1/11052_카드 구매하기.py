cc8ed7d2122443e0aa01e11f0276f024c0c2589e
권도건 <rnsjquddbs@gmail.com>
2022-09-05 22:04:42 +0900

    BOJ Problem Solving

---
+++ BAEKJOON/다이나믹 프로그래밍 1/11052_카드 구매하기.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+# 최대값과 인덱스를 같이 비교하기 위해 앞에 [0] 추가
+pack = [0] + list(map(int, sys.stdin.readline().split()))
+max_num = [0] * (n + 1)
+
+max_num[1] = pack[1]
+
+# max_num[1]은 pack[1]과 같다.
+# max_num[2]은 pack[2] or max_num[1]+pack[1]
+# max_num[3]은 pack[3] or max_num[2]+pack[1] or max_num[1]+pack[2]
+# max_num[4]은 pack[4] or max_num[3]+pack[1] or max_num[1]+pack[3] or max_num[2]+pack[2] or max_num[2]+pack[2]
+for i in range(2, n+1):
+    for j in range(1, i+1):
+        if max_num[i] < max_num[i-j] + pack[j]:
+            max_num[i] = max_num[i-j] + pack[j]
+
+print(max_num[n])
