38acf5e98b60a6e88c3a00e336ac61ffd6aa17a3
권도건 <rnsjquddbs@gmail.com>
2022-09-17 01:16:05 +0900

    BOJ Problem Solving

--- BAEKJOON/다이나믹 프로그래밍 1/14002_가장 긴 증가하는 부분 수열 4.py
+++ BAEKJOON/다이나믹 프로그래밍 1/14002_가장 긴 증가하는 부분 수열 4.py
@@ -6,27 +6,28 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 dp = [0] * n
+dp[0] = 1
 
-res = [[] for _ in range(n)]
-tmp = 0
-ans = []
-
-for i in range(n):
+# 가장 긴 증가하는 부분 수열 dp에 정리
+for i in range(1, n):
     for j in range(i):
-        if arr[j] < arr[i] and dp[j] > dp[i]:
+        if dp[j] > dp[i] and arr[j] < arr[i]:
             dp[i] = dp[j]
     dp[i] += 1
-    if dp[i] == 1:
-        res[tmp].append(arr[i])
-        tmp += 1
-    else:
-        for k in range(n):
-            if res[k] == []:
-                break
-            elif res[k][-1] < arr[i]:
-                res[k].append(arr[i])
-                if len(res[k]) > len(ans):
-                    ans = res[k]
 
-print(len(ans))
-print(*ans)
+# 길이가 가장 긴 수열의 자리 확인
+max_idx = dp.index((max(dp)))
+# 임의의 값에 저장
+tmp = max_idx
+# 결과를 저장할 리스트
+res = [arr[max_idx]]
+
+# 뒤에서부터 가장 큰 길이 - 1 한 것의 dp값을 확인 후 res에 저장
+for k in range(max_idx-1, -1, -1):
+    if dp[k] == dp[tmp] - 1:
+        res.append(arr[k])
+        tmp = k
+
+print(max(dp))
+res.reverse()
+print(*res)
