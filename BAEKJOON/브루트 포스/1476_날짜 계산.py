96bfdacd5f75c596999a7a2ab9566b66a3a2c44f
권도건 <rnsjquddbs@gmail.com>
2022-09-26 00:12:28 +0900

    BOJ Problem Solving

--- BAEKJOON/브루트 포스/1476_날짜 계산.py
+++ BAEKJOON/브루트 포스/1476_날짜 계산.py
@@ -2,3 +2,21 @@ import sys
 
 e, s, m = map(int, sys.stdin.readline().split())
 
+res = 1
+ee, ss, mm = 1, 1, 1
+
+while True:
+    if ee == e and ss == s and mm == m:
+        print(res)
+        break
+    else:
+        ee += 1
+        ss += 1
+        mm += 1
+        res += 1
+        if ee > 15:
+            ee -= 15
+        if ss > 28:
+            ss -= 28
+        if mm > 19:
+            mm -= 19
