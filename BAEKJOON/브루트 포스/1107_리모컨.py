32e59881e0eba03efcdf3491af8e85e2bb3e52a3
권도건 <rnsjquddbs@gmail.com>
2022-09-28 00:42:13 +0900

    BOJ Problem Solving

--- BAEKJOON/브루트 포스/1107_리모컨.py
+++ BAEKJOON/브루트 포스/1107_리모컨.py
@@ -2,24 +2,43 @@ import sys
 
 input = sys.stdin.readline
 
-n = str(input().strip())
-m = int(input().strip())
+n = str(sys.stdin.readline().strip())
+m = int(sys.stdin.readline().strip())
 arr = [1] * 10
 if m:
-    broken = list(map(int, input().strip().split()))
+    broken = list(map(int, sys.stdin.readline().strip().split()))
     for i in broken:
         arr[i] = 0
 
+up_down = 0
 tmp = ''
 for i in n:
     s = 20
     plus = 0
-    for j in range(10):
-        if arr[j]:
-            absolute_number = abs(int(i) - j)
-            if s >= absolute_number:
-                s = absolute_number
+    if up_down == 0:
+        for j in range(10):
+            if arr[j]:
+                absolute_number = abs(int(i) - j)
+                if s >= absolute_number:
+                    s = absolute_number
+                    plus = str(j)
+        if plus:
+            if int(plus) > int(i):
+                up_down = 2
+            elif int(plus) < int(i):
+                up_down = 1
+
+    elif up_down == 1:
+        for j in range(9, -1, -1):
+            if arr[j]:
                 plus = str(j)
+                break
+
+    else:
+        for j in range(10):
+            if arr[j]:
+                plus = str(j)
+                break
     if plus:
         tmp += plus
 
