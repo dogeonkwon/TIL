77126cc1958fc361069b0f8978db60283a73825f
권도건 <rnsjquddbs@gmail.com>
2022-10-05 00:56:25 +0900

    BOJ Problem Solving

--- BAEKJOON/브루트 포스/14500_테트로미노.py
+++ BAEKJOON/브루트 포스/14500_테트로미노.py
@@ -1,19 +1,49 @@
-import sys, collections
+import sys
 
-n, m = map(int, sys.stdin.readline().split())
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-arr = list(list(map(int, sys.stdin.readline().strip().split())) for _ in range(n))
+N, M = map(int, input().split())
+graph = [[0 for _ in range(M)] for _ in range(N)]
+check = [[0 for _ in range(M)] for _ in range(N)]
+max_val = 0
+for i in range(N):
+    row = list(map(int, input().split()))
+    for j, r in enumerate(row):
+        graph[i][j] = r
+        if max_val < r:
+            max_val = r
 
-res = 0
-for i in range(n):
-    for j in range(m):
-         q = collections.deque([[[i, j]]])
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-         while q:
-             v = q.popleft()
-             print(v)
-             if len(v) == 4:
-                 if arr[v[0][0]][v[0][1]] + arr[v[1][0]][v[1][1]] + arr[v[2][0]][v[2][1]] + arr[v[3][0]][v[3][1]] > res:
-                     res = arr[v[0][0]][v[0][1]] + arr[v[1][0]][v[1][1]] + arr[v[2][0]][v[2][1]] + arr[v[3][0]][v[3][1]]
-             for k in (0, 1), (1, 0), (0, -1), (-1, 0):
-                 v[]
+sol = 0
+
+
+def dfs(y, x, depth, s):
+    global sol
+    if depth == 4:
+        sol = max(s, sol)
+        return False
+    if s + (4 - depth) * max_val <= sol:
+        return False  # 나머지 깊이 값이 모두 최대값이라도 갱신 불가능한 경우
+    for d in range(4):
+        ny, nx = y + dy[d], x + dx[d]
+        if 0 <= ny < N and 0 <= nx < M and check[ny][nx] == 0:
+            if depth == 2:  # 'ㅏ', 'ㅓ' 모양의 가운데 지점인 경우 (깊이 2)
+                check[ny][nx] = 1  # 깊이 3의 지점 방문 처리
+                # 깊이 2 지점에서 다시 탐색해서 깊이 4 지점 탐색
+                dfs(y, x, depth + 1, s + graph[ny][nx])
+                check[ny][nx] = 0
+            check[ny][nx] = 1
+            dfs(ny, nx, depth + 1, s + graph[ny][nx])
+            check[ny][nx] = 0
+    return False
+
+
+for i in range(N):
+    for j in range(M):
+        check[i][j] = 1
+        dfs(i, j, 1, graph[i][j])
+        check[i][j] = 0
+print(sol)
--- BAEKJOON/브루트 포스/6064_카잉 달력.py
+++ BAEKJOON/브루트 포스/6064_카잉 달력.py
@@ -1,24 +1,20 @@
-import sys
+import sys, math
 
 t = int(sys.stdin.readline())
 for _ in range(t):
     k = 0
     m, n, x, y = map(int, sys.stdin.readline().split())
-    i = j = 0
-    stop = 1
-    while stop:
-        i = (i % m) + 1
-        j = (j % n) + 1
-        k += 1
 
-        if i == x:
-            if j == y:
-                print(k)
-                stop = 0
-                break
+    # 규칙은 생각보다 단순
+    # x, y가 최소공배수 보다 커지지 않도록
+    # x += m, y += n 하면서 조건에 만족하는 숫자가 있는지 확인
+    min_num = m*n // math.gcd(m, n)
 
-        if i == m:
-            if j == n:
-                print(-1)
-                stop = 0
-                break
+    ans = 0
+    while x <= min_num:
+        if not (x - y) % n:     # not (x - y) % n 이라는 조건을 생각하는 것이 어려웠다.
+            print(x)
+            ans = 1
+        x += m
+    if not ans:
+        print(-1)
