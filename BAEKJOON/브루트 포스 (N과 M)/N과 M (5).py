85d500924e6ed375aa4d3ea8ecfd7481c6ab2741
권도건 <rnsjquddbs@gmail.com>
2022-10-10 14:31:58 +0900

    BOJ Solving

--- BAEKJOON/브루트 포스 (N과 M)/N과 M (5).py
+++ BAEKJOON/브루트 포스 (N과 M)/N과 M (5).py
@@ -1,4 +1,9 @@
-import sys
+import sys, itertools
 
 n, m = map(int, sys.stdin.readline().split())
+lst = sorted(list(map(int, sys.stdin.readline().split())))
 
+res = list(itertools.permutations(lst, m))
+
+for i in res:
+    print(*i)
