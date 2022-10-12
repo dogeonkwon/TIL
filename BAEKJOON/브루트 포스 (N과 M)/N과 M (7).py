3e21ab987fc363f275d59f89166e84fe640b6377
권도건 <rnsjquddbs@gmail.com>
2022-10-13 01:02:27 +0900

    BOJ Solving

--- BAEKJOON/브루트 포스 (N과 M)/N과 M (7).py
+++ BAEKJOON/브루트 포스 (N과 M)/N과 M (7).py
@@ -1,10 +1,22 @@
-import sys, itertools
+import sys
+
+
+def pick(n, bucket, k):
+    global arr
+
+    if k == 0:
+        print(*bucket)
+        return
+
+    lastidx = len(bucket) - k
+
+    for i in range(0, n):
+        bucket[lastidx] = arr[i]
+        pick(n, bucket, k-1)
+
 
 n, m = map(int, sys.stdin.readline().split())
 
 arr = sorted(list(map(int, sys.stdin.readline().split())))
 
-res = list(itertools.combinations_with_replacement(arr, m))
-
-for i in res:
-    print(*i)
+pick(n, [0]*m, m)
