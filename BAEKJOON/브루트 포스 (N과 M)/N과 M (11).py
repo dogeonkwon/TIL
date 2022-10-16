a596c99b57077fa368bf24bb4e0d35d3fd0c24ab
권도건 <kjmk1007@naver.com>
2022-10-16 17:56:49 +0900

    BOJ Solving

---
+++ BAEKJOON/브루트 포스 (N과 M)/N과 M (11).py
@@ -0,0 +1,10 @@
+import sys, itertools
+
+n, m = map(int, sys.stdin.readline().split())
+
+arr = sorted(list(map(int, sys.stdin.readline().split())))
+
+res = sorted(set(list(itertools.combinations(arr, m))))
+
+for i in res:
+    print(*i)
