65888addf92fab73134cfd5530c690bfe113cf92
권도건 <rnsjquddbs@gmail.com>
2022-11-25 12:03:14 +0900

    Solving

--- Algorithm/Programmers/n^2 배열 자르기.py
+++ Algorithm/Programmers/n^2 배열 자르기.py
@@ -1,14 +1,7 @@
 def solution(n, left, right):
     answer = []
-    k = 1
 
-    for i in range(n):
-        for j in range(n):
-            if left <= (i * n) + j <= right:
-                if j + 1 <= k:
-                    answer.append(k)
-                else:
-                    answer.append(j + 1)
-        k += 1
+    for i in range(left, right + 1):
+        answer.append(max(i // n, i % n) + 1)
 
     return answer
