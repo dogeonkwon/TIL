66fb0d5076aa2ff1bd369183ab4a8d49ba65af71
권도건 <rnsjquddbs@gmail.com>
2022-11-29 17:46:32 +0900

    Solving

---
+++ Algorithm/Programmers/전화번호 목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    answer = True
+    n = len(phone_book)
+
+    arr = sorted(phone_book)
+    for i in range(n - 1):
+        m = len(arr[i])
+        if arr[i] == arr[i + 1][0:m]:
+            answer = False
+            break
+
+    return answer
