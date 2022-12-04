a0906f266ab58c8737f1e1aa49074d3148d4d65f
권도건 <rnsjquddbs@gmail.com>
2022-12-04 16:13:28 +0900

    Solving

--- Algorithm/Programmers/k진수에서 소수 개수 구하기.py
+++ Algorithm/Programmers/k진수에서 소수 개수 구하기.py
@@ -1,14 +1,25 @@
+def prime(s):
+    ans = True
+    if s < 2:
+        ans = False
+    else:
+        for i in range(2, int(s ** 0.5) + 1):
+            if not s % i:
+                ans = False
+    return ans
+
+
 def solution(n, k):
     answer = 0
 
-    # 소수 리스트
-    prime_lst = [True] * 1000001
-    prime_lst[0] = prime_lst[1] = False
+    #     # 소수 리스트 만들기
+    #     prime_lst = [True] * 1000001
+    #     prime_lst[0] = prime_lst[1] = False
 
-    for i in range(2, int(1000001 ** 0.5) + 1):
-        if prime_lst[i] == True:
-            for j in range(i * i, 1000001, i):
-                prime_lst[j] = False
+    #     for i in range(2, int(1000001 ** 0.5) + 1):
+    #         if prime_lst[i] == True:
+    #             for j in range(i*i, 1000001, i):
+    #                 prime_lst[j] = False
 
     # 10진수 -> n진수로 변환
     transfer = ''
@@ -23,11 +34,16 @@ def solution(n, k):
     tmp = ''
 
     for i in range(m):
-        if res[i] == '0' or res[i] == '1':
-            if prime_lst[int(tmp)]:
+        if res[i] == '0':
+            if tmp != '' and prime(int(tmp)):
                 answer += 1
             tmp = ''
         else:
             tmp += res[i]
 
+    if tmp != '' and prime(int(tmp)):
+        answer += 1
+
     return answer
+
+
