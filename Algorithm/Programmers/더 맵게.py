3252fe6743aec8d845921d970314cd45ab9a04f7
권도건 <rnsjquddbs@gmail.com>
2022-12-13 05:25:07 +0900

    Solving

--- Algorithm/Programmers/더 맵게.py
+++ Algorithm/Programmers/더 맵게.py
@@ -1,3 +1,14 @@
-def solution(fees, records):
-    answer = []
+def solution(scoville, K):
+    answer = 0
+    scoville.sort(reverse=True)
+    n = len(scoville)
+
+    while scoville:
+        if scoville[-1] >= K:
+            break
+        answer += 1
+        tmp = scoville.pop() + scoville.pop() * 2
+        scoville.append(tmp)
+        scoville.sort(reverse=True)
+
     return answer
